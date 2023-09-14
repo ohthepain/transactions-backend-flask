04283b0cba24bca6254cefcc2a420b95e6933625
Paul Wilkinson <cremoni@gmail.com>
2023-09-14 18:15:02 +0200

    CORS

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 import uuid
 from datetime import datetime
-from flask import Flask, render_template, request
-from flask_restx import Api, Resource, fields
+from flask import Flask
+from flask_restx import Api
+from flask_cors import CORS
 from resource.ping import api as ping
 from resource.transaction import api as transaction
 from resource.account import api as accounts
@@ -9,6 +10,7 @@ from resource.account import api as accounts
 print("Reticulating splines ...")
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app, version="1.0", title="Transaction API", description="Transaction API for Alva Labs Test", doc="/swagger")
 api.add_namespace(ping, "/")
 api.add_namespace(transaction, "/")
